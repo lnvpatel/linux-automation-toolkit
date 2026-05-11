cd40618fd0da3d9fd8fece7e329186e267a26267
Vatsalya Patel <lnvpatel@gmail.com>
2026-05-11 08:12:15 +0000

    Add Linux user create automation script

--- user-management/create_user.sh
+++ user-management/create_user.sh
@@ -1,1 +1,61 @@
+#!/bin/bash
+
+# ==========================================
+
+# Linux User Creation Script
+
+# Author: Vatsalya Patel
+
+# Description:
+
+# Automates Linux user creation and
+
+# validates account existence.
+
+# ==========================================
+
+echo "=========================================="
+echo " Linux User Creation Report"
+echo "=========================================="
+echo "Hostname: $(hostname)"
+echo "Date: $(date)"
+echo ""
+
+# Input username
+
+read -p "Enter new username: " USERNAME
+
+echo ""
+
+# Check if user already exists
+
+if id "$USERNAME" &>/dev/null; then
+echo "Status : User already exists."
+exit 1
+fi
+
+# Create user
+
+sudo useradd -m "$USERNAME" 2>/dev/null
+
+# Verify user creation
+
+if id "$USERNAME" &>/dev/null; then
+echo "Status : User created successfully."
+echo "Username : $USERNAME"
+
+
+echo ""
+echo "Home Directory"
+echo "------------------------------------------"
+
+eval echo "~$USERNAME"
+
+
+else
+echo "Status : User creation failed."
+fi
+
+echo ""
+echo "User creation operation completed."
 
