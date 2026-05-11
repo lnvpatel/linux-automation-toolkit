f402ba5ad0ce13ea5807f40df15983178c7341e4
Vatsalya Patel <lnvpatel@gmail.com>
2026-05-11 07:36:59 +0000

    Add disk monitoring script and execution screenshot

--- monitoring/disk_monitor.sh
+++ monitoring/disk_monitor.sh
@@ -1,1 +1,52 @@
+#!/bin/bash
 
+# ==========================================
+
+# Linux Disk Monitoring Script
+
+# Author: Vatsalya Patel
+
+# Description:
+
+# Monitors disk usage and displays
+
+# warning alerts if usage exceeds threshold.
+
+# ==========================================
+
+# Warning threshold percentage
+
+THRESHOLD=80
+
+echo "=========================================="
+echo " Disk Usage Monitoring Report"
+echo "=========================================="
+echo "Hostname: $(hostname)"
+echo "Date: $(date)"
+echo ""
+
+# Read disk usage information
+
+df -h --output=source,pcent,target | tail -n +2 | while read filesystem usage mountpoint
+do
+# Remove % symbol
+usage_value=$(echo "$usage" | sed 's/%//')
+
+
+echo "Partition   : $filesystem"
+echo "Mounted On  : $mountpoint"
+echo "Usage       : $usage"
+
+# Threshold check
+if [ "$usage_value" -ge "$THRESHOLD" ]; then
+    echo "Status      : WARNING - High Disk Usage!"
+else
+    echo "Status      : OK"
+fi
+
+echo "------------------------------------------"
+
+
+done
+
+echo "Disk monitoring completed."
